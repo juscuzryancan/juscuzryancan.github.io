cee544454f6ca1bd9cbdbd91e17f6721827e953a
Ryan Riley Puzon <rrgpuzon@gmail.com>
2021-02-09 13:30:07 -0800

    starting navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom'
 
 const App = () => {
     return (
-        <div>Hello World</div>
+        <Router>
+            <Route exact path='/'>
+
+            </Route>
+        </Router>
     );
 }
 
