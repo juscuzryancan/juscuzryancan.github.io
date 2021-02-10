a7843303b02ba5a05686926860fb56877783fa7a
Ryan Riley Puzon <rrgpuzon@gmail.com>
2021-02-09 20:02:55 -0800

    adding navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
+
 import {
     BrowserRouter as Router,
     Route
 } from 'react-router-dom'
 
+import {
+    Nav
+} from '../';
+
 const App = () => {
     return (
         <Router>
+            <Nav></Nav>
             <Route exact path='/'>
 
             </Route>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,11 @@
 import Navbar from 'react-bootstrap/Navbar';
 
 const Nav = () => {
-
+    return (
+        <Navbar>
+            Ryan Riley Puzon
+        </Navbar>
+    );
 };
 
 export default Nav;
