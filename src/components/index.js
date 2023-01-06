4dd2364dda29cf84bcd507f0606e270020742f68
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-01-05 20:52:59 -0800

    deletes src

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Navigation} from './Navigation';
-export {default as Hero} from './Hero';
-export {default as Skills} from './Skills';
-export {default as Projects} from './Projects';
-export {default as Footer} from './Footer';
