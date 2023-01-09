43f60b32b200ba2e2016d55c66a28f2232d51955
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-01-09 10:49:16 -0800

    adds base prefix

--- next.config.js
+++ next.config.js
@@ -2,17 +2,20 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = '';
+let basePrefix = '/';
 
 if (isGithubActions) {
   // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
   assetPrefix = `/${repo}/`
+  basePrefix = `/${repo}`
 }
 
 
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix,
+  basePrefix,
   images: {
     unoptimized: true
   },
